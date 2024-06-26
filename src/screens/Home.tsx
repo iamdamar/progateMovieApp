f58fec0e99a044a9fd97e3be2b56e80bbc92ea80
damarwu <hello.damarwu@gmail.com>
2024-06-26 20:25:23 +0700

    set up bottom tab navigation

---
+++ src/screens/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+export default function Home(): JSX.Element {
+    return (
+        <View>
+            <Text>
+                Home
+            </Text>
+        </View>
+    )
+}
